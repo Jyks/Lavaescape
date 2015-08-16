8bb704ab95ada1a25059e59e42387453f6c95055
Jyks <zyxfin@gmail.com>
2015-08-16 13:58:29 +0300

    Update LavaEscape.java

--- src/com/zyxum/lavaescape/LavaEscape.java
+++ src/com/zyxum/lavaescape/LavaEscape.java
@@ -9,7 +9,7 @@ public class LavaEscape extends StateBasedGame {
 
 	public static boolean debug = false;
 	
-	public static final int game=1;
+	public static final int game = 1;
 	
 	public LavaEscape() {
 		super("Lava Escape");
@@ -22,15 +22,14 @@ public class LavaEscape extends StateBasedGame {
 	}
 	
 	public static void main(String[] args) throws SlickException {
-		if(args!=null && !debug){
-			if(args.toString().contains("debug")) debug=true;
-		}
-        AppGameContainer app = new AppGameContainer(new LavaEscape());
-        app.setShowFPS(false);
-        app.setDisplayMode(640, 480, false);
-        app.setAlwaysRender(true);
-        app.setUpdateOnlyWhenVisible(true);
-        app.setVSync(true);
-        app.start();
-   }
+		if(args!=null && !debug) if(args.toString().contains("debug")) debug=true;
+		
+		AppGameContainer app = new AppGameContainer(new LavaEscape());
+		app.setShowFPS(false);
+		app.setDisplayMode(640, 480, false);
+		app.setAlwaysRender(true);
+		app.setUpdateOnlyWhenVisible(true);
+		app.setVSync(true);
+		app.start();
+   	}
 }
